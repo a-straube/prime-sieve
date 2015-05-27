66c38995318ce2c311e730e69a68c8889d7f9de5
Ben Cornelis & Axel Straube <student@epicodus.com>
2015-05-27 15:46:06 -0700

    write prime sieve method

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,35 @@ var numbersBefore = function(n) {
   };
   return numbers;
 };
+
+var deleteValue = function(array, value) {
+  var i = array.indexOf(value);
+  if (i >= 0) {
+    array.splice(i, 1);
+  }
+  return array;
+};
+
+var primesBefore = function(n) {
+  var numbers = numbersBefore(n);
+  var primes = numbers.slice();
+  numbers.forEach (function(number) {
+    // delete all multiples of number
+    var multiple = 2;
+    while (number*multiple <= n) {
+      deleteValue(primes, number*multiple);
+      multiple += 1;
+    }
+  });
+  return primes;
+};
+
+$(document).ready(function() {
+  $("form#numbercounter").submit(function(event) {
+    var number = parseInt($("input#number").val());
+
+    var numberList = numbersBefore(number).map (function(x) {return x.toString();})
+    $("#result").text('Hover over a number to see if it is prime: ' + numberList.join(', '));
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,24 @@ describe('numbersBefore', function() {
   it('returns an array of integers starting at 2 up to a given number', function() {
     expect(numbersBefore(10)).to.eql([2,3,4,5,6,7,8,9,10]);
   });
-})
+});
+
+describe('deleteValue', function() {
+  it('removes a value from an array', function() {
+    expect(deleteValue([2,3,4,5,6,7,8,9,10], 5)).to.eql([2,3,4,6,7,8,9,10]);
+  });
+});
+
+describe('primesBefore', function() {
+  it('returns an array containing 2 for any number greater than or equal to 2', function() {
+    expect(primesBefore(2)).to.eql([2]);
+  });
+
+  it('returns an array containing all prime numbers up to a given number', function() {
+    expect(primesBefore(10)).to.eql([2,3,5,7]);
+  });
+
+  it('returns an array containing all prime numbers up to a given number', function() {
+    expect(primesBefore(25)).to.eql([2,3,5,7,11,13,17,19,23]);
+  });
+});
